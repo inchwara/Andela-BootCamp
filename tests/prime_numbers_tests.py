fc336a0c3195b8611be39b08b08ae7bf8bad0619
inchwara <inchwara@gmail.com>
2017-02-06 17:37:15 +0300

    changes

---
+++ tests/prime_numbers_tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+from prime_numbers import prime_numbers
+
+class TestPrimeNumbers(unittest.Testcase)
+
+	def test_prime_numbers(self):
+	self.assertEqual(prime_numbers.prime_numbers(20)[2,3,5,7,11,13,17,19])
+
+
+
+
