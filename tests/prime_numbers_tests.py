25d423dd563e66c6c0dc1f22be5e295567046d2f
unknown <Emmanuel@EMMANUEL-PC.questholdings.biz>
2017-02-07 07:38:23 +0300

    test case 2

--- tests/prime_numbers_tests.py
+++ tests/prime_numbers_tests.py
@@ -4,7 +4,12 @@ from prime_numbers *
 
 class TestPrimeNumbers(unittest.TestCase)
         def test_prime_numbers(self):
-	self.assertEqual(prime_numbers.prime_numbers(20)[2,3,5,7,11,13,17,19])
+			self.assertEqual(prime_numbers.prime_numbers(20)[2,3,5,7,11,13,17,19])
+
+		def test_prime_numbers(self):
+			if n < 9: return True
+
+		
 
 
 
