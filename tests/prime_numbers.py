c5ac8f09c99a7306cea738d41362d1b162ab463e
unknown <Emmanuel@EMMANUEL-PC.questholdings.biz>
2017-02-07 07:32:06 +0300

    new update

---
+++ tests/prime_numbers.py
@@ -0,0 +1,8 @@
+def getPrime(n):
+	for x in range(0,n):
+		if n%x == 0:
+			answer = False
+
+    	if x == n:
+    		print x
+
--- tests/prime_numbers_tests.py
+++ tests/prime_numbers_tests.py
@@ -1,10 +1,9 @@
 import unittest
 
-from prime_numbers import prime_numbers
+from prime_numbers *
 
-class TestPrimeNumbers(unittest.Testcase)
-
-	def test_prime_numbers(self):
+class TestPrimeNumbers(unittest.TestCase)
+        def test_prime_numbers(self):
 	self.assertEqual(prime_numbers.prime_numbers(20)[2,3,5,7,11,13,17,19])
 
 
